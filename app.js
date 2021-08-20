7cf0606d79b294357ff260188114f2593990cfd3
Eddie <eddie.echeverria94@gmail.com>
2021-08-19 20:22:30 -0700

    utilized exports and promises

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage (name,github);
+const pageHTML = generatePage (name,github);
 
-// fs.writeFile('index.html', pageHTML, err => {
-//     if (err) throw err;
+fs.writeFile('./dist/index.html', pageHTML, err => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('Page created! Check out index.html in this direction to see it!');
 
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('Style sheet copied successfully!');
+    });
+});
 
 
 const promptUser = () => {
@@ -137,4 +147,17 @@ promptUser()
     .then(promptProject)
     .then(portfolioData => {
         console.log(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
